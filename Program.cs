0f436fbbda8d57a2e8831f801631bb146322d716
dhruvpatel7014 <100025899+dhruvpatel7014@users.noreply.github.com>
2022-02-19 17:22:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pra1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int temp = num;
+            while (temp > 0) 
+            {
+              int rem = num % 10;
+                sum += rem;
+                temp = temp / 10;
+
+              
+            }
+            Console.WriteLine("summation of number is" + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
